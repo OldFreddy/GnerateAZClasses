c28f697e1c06308ebb28bc6e9fa4d0696c7ecee4
Mikhail Rybkin <oldfredddy@gmail.com>
2021-01-13 16:42:51 +0300

    Initial commit

---
+++ runner/src/main/java/com/school/Main.java
@@ -0,0 +1,10 @@
+package com.school;
+
+import com.character.api.CharApi;
+
+public class Main {
+    public static void main(String[] args) {
+        CharApi a = new H();
+        a.printMe(); //1:29 закончил на видео
+    }
+}
